87d6e3aad2e279a5abed537a1eaa1bab717b96b6
Steve <84150915+co-sneff@users.noreply.github.com>
2021-06-23 14:43:53 -0400

    changed interface
    made eth1 primary and added SG

--- compute.tf
+++ compute.tf
@@ -72,9 +72,10 @@ resource "ibm_is_instance" "vnf_vsi" {
   network_interfaces {
     name = "eth0"
     subnet = data.ibm_is_subnet.vnf_subnet1.id
+    security_groups = [ibm_is_security_group.vnf_security_group.id]
   }
   
-  primary_network_interface {
+  network_interfaces {
     name   = "eth1"
     subnet = data.ibm_is_subnet.vnf_subnet2.id
     security_groups = [ibm_is_security_group.vnf_security_group.id]
