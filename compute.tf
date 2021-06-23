f6ccd88a2aea573b8cca9159d595162d064ae1cd
Steve <84150915+co-sneff@users.noreply.github.com>
2021-06-23 14:58:27 -0400

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -75,7 +75,7 @@ resource "ibm_is_instance" "vnf_vsi" {
     security_groups = [ibm_is_security_group.vnf_security_group.id]
   }
   
-  secondary_network_interface {
+  primary_network_interface {
     name   = "eth1"
     subnet = data.ibm_is_subnet.vnf_subnet2.id
     security_groups = [ibm_is_security_group.vnf_security_group.id]
